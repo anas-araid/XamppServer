dcc57052ecd52e6abf703ab6e1192b29da814b1a
asdf1899 <araid.anas99@gmail.com>
2018-10-19 12:27:24 +0200

    Init project

---
+++ XamppServer/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace XamppServer
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 221);
+            this.Name = "frmServer";
+            this.Text = "Xampp Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
